e07b27f4e983f6e02557eb72423cb285780b70cc
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-11 00:38:13 +0100

    Added a go home button

--- public/app.js
+++ public/app.js
@@ -2,10 +2,10 @@ const initialize = function() {
 
   const container = document.getElementById('main-map');
   const myHouse = {lat: 56.1703, lng: -4.02793};
-  const inverardochMains = {lat: 56.1503, lng: -4.12793};
+  const inverardochMains = {lat: 56.181945, lng: -4.043252};
   const glasgow = {lat: 55.8651, lng: -4.258};
   const edinburgh = {lat:55.953251, lng: -3.188267};
-  const zoom = 9;
+  const zoom = 13;
 
   const map = new MapWrapper(container, myHouse, zoom);
 
@@ -17,6 +17,9 @@ const initialize = function() {
 
   const gotoEdinburghButton = document.getElementById('button-goto-edinburgh');
   map.gotoCity(gotoEdinburghButton, edinburgh);
+
+  const gotoHomeButton = document.getElementById('button-goto-home');
+  map.gotoCity(gotoHomeButton, myHouse);
 }
 
 window.addEventListener('DOMContentLoaded', initialize);
