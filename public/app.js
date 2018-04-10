2cb6c997a1b16a315aeb178597ece7fb8b5a2ac5
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-10 19:11:29 +0100

    Added first attempt at showing an InfoWindow, using mouseover

--- public/app.js
+++ public/app.js
@@ -1,10 +1,16 @@
 const initialize = function() {
 
   const container = document.getElementById('main-map');
-  const center = {lat: 56.1703, lng: -4.02793};
+  const myHouse = {lat: 56.1703, lng: -4.02793};
+  const inverardochMains = {lat: 56.1503, lng: -4.12793};
   const zoom = 9;
 
-  const map = new MapWrapper(container, center, zoom);
+  const map = new MapWrapper(container, myHouse, zoom);
+
+  map.addMarker(myHouse);
+  map.addMarker(inverardochMains);
+
+  map.showInfoEvent();
 }
 
 window.addEventListener('DOMContentLoaded', initialize);
--- public/mapWrapper.js
+++ public/mapWrapper.js
@@ -3,4 +3,39 @@ const MapWrapper = function(container, coords, zoom){
     center: coords,
     zoom: zoom
   });
+  this.markers = [];
 }
+
+MapWrapper.prototype.addMarker = function (coords) {
+  const marker = new google.maps.Marker({
+    position: coords,
+    map: this.googleMap
+  });
+  this.markers.push(marker);
+};
+
+// MapWrapper.prototype.addClickEvent = function () {
+//   google.maps.event.addListener(this.googleMap, 'click', function(event){
+//   const coords = {lat: event.latLng.lat(), lng: event.latLng.lng()};      // where I have clicked:
+//   this.addMarker(coords);                                                 // add a marker
+//   }.bind(this));
+// };
+
+// var contentString = "Testing";
+//
+// var infowindow = new google.maps.InfoWindow({
+//     content: contentString
+// });
+
+MapWrapper.prototype.showInfoEvent = function () {
+  console.log("mouseover test");
+  for(marker of this.markers){
+    google.maps.event.addListener(marker, 'mouseover', function(event){
+    console.log(event.latLng.lat());
+    var infowindow = new google.maps.InfoWindow({
+      content: "Test"
+    });
+    infowindow.open(this.googleMap, marker);
+    }.bind(this));
+  }
+};
